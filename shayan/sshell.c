a399d8f383c14297dc452c241e455e1574cad545
Shayan Mandegarian <smandegarian@yahoo.com>
2019-10-11 00:46:42 -0700

    phase 3

--- shayan/sshell.c
+++ shayan/sshell.c
@@ -5,6 +5,14 @@
 #include <sys/wait.h>
 #include <string.h>
 
+struct input
+{
+	char input[512];
+	char* command[1];
+	char* arguments[16];
+
+};
+
 int main(int argc, char *argv[])
 {
 	int retval;
@@ -13,33 +21,66 @@ int main(int argc, char *argv[])
 	pid_t pid;
 	
 	cmd[0] = (char*)malloc(size);
-	
+	struct input *userIn = malloc(sizeof(struct input));
 
 	printf("sshell$ ");
-	getline(&cmd[0], &size, stdin);
-	cmd[0] = strtok(cmd[0], " \n");
-	cmd[1] = NULL;
-	/*
-	int counter = 0;
-	while (tokenized != NULL)
+	int lineSize = getline(&cmd[0], &size - 1, stdin);
+	if (lineSize < 0)
 	{
-		inputArray[counter] = (char*)malloc(strlen(tokenized + 1));
-		inputArray[counter] = tokenized;
-		tokenized = strtok(userInput, " ");
+		perror("getline");
+		exit(1);
+	}
+	
+	cmd[0] = strtok(cmd[0], "\n");
+	if (cmd[0] == NULL)
+        {
+		//free(cmd[0]);
+		//free(userIn);
+                main(argc, argv);
+        }
+	strcpy(userIn->input, cmd[0]);
+	//cmd[1] = NULL;
+	userIn->arguments[15] = NULL;
+
+	int counter = 0;
+	cmd[0] = strtok(userIn->input, " ");
+	while (cmd[0] != NULL)
+	{
+		//cmd[0] = strtok(NULL, " ");
+		//printf("cmd[0]: %s\n", cmd[0]);
+		if (counter == 0)
+		{
+			userIn->command[counter] = cmd[0];
+			userIn->arguments[counter] = cmd[0];
+		}
+		else if (counter > 16)
+		{
+			fprintf(stderr, "Error: too many process arguments\n");
+			//free(cmd[0]);
+			//free(userIn);
+			main(argc, argv);
+		}
+		else
+		{
+			userIn->arguments[counter] = cmd[0];
+		}
+		cmd[0] = strtok(NULL, " ");
 		counter++;
 	}
-	*/
+	
 
 	pid = fork();
 	if(pid == 0)
 	{
-		execvp(cmd[0], cmd);
+		//execvp(cmd[0], cmd);
+		//printf("command: %s\n", userIn->command[0]);
+		execvp(userIn->command[0], userIn->arguments);
 		exit(1);
 	}
 	else if(pid != 0)
 	{
 		waitpid(-1, &retval, 0);
-		fprintf(stderr, "+ completed '%s' [%d]\n", cmd[0], retval);
+		fprintf(stderr, "+ completed '%s' [%d]\n", userIn->input, retval);
 		main(argc, argv); // Restart so user can continue inputting commands
 	}
 
