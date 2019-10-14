edf8665d49a0ba9eab66c25924d88d9db2fe6857
farache <farache@ad3.ucdavis.edu@pc1.cs.ucdavis.edu>
2019-10-13 22:10:00 -0700

    finished phase 3

--- farache/sshell.c
+++ farache/sshell.c
@@ -1,30 +1,52 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
 #include <sys/wait.h>
+
+typedef struct parsedCmd parsedCmd;
+
+struct parsedCmd {
+	char *cmd[16]; 
+};
+
+void parseCmd(struct parsedCmd *obj, char *cmd){
+	int i = 0;
+	char *temp = strtok(cmd, " ");
+	
+	while(temp != NULL){
+		obj->cmd[i++] = temp;
+		temp = strtok(NULL, " ");
+	}
+	obj->cmd[i] = NULL;
+}
 
 int main(int argc, char *argv[])
 {
+	pid_t pid;
+	char *cmd;
+	size_t bufsize = 512;
+	int status;
+	int len;
+	char *origCmd;
+	parsedCmd pCmd;
+	cmd = (char *) malloc(bufsize * sizeof(char));
+	
 	while(1){
-		pid_t pid;
-		char *cmd[2];
-		size_t bufsize = 512;
-		int status;
-
-		cmd[0] = (char *) malloc(bufsize * sizeof(char));
 		fprintf(stdout, "sshell$ ");
-		int len = getline(&cmd[0], &bufsize, stdin);
-		cmd[0][len - 1] = '\0';
-		cmd[1] = NULL;
+		len = getline(&cmd, &bufsize, stdin);
+		cmd[len - 1] = '\0';
+		origCmd = strdup(cmd);
+		parseCmd(&pCmd, cmd);
 		pid = fork();
 
 		if(pid == 0){
-			execvp(cmd[0], cmd);
-			perror("execvp");
+			execvp(pCmd.cmd[0], pCmd.cmd);
+			perror("Error");
 			exit(1);
 		} else {
 			waitpid(-1, &status, 0);
-			fprintf(stderr, "+ completed '%s' [%d]\n", cmd[0], WEXITSTATUS(status));
+			fprintf(stderr, "+ completed '%s' [%d]\n", origCmd, WEXITSTATUS(status));
 		}
 	}
 }
