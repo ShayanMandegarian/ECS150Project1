9de04eb5899c8d0cdc6f2833169199c8538ab252
farache <farache@ad3.ucdavis.edu@pc1.cs.ucdavis.edu>
2019-10-15 22:10:17 -0700

    pipes commented out

--- shayan/sshell.c
+++ shayan/sshell.c
@@ -7,8 +7,8 @@
 #include <string.h>
 #include <limits.h>
 #include <fcntl.h>
-
-//const int PATH_MAX = 512;
+#include <ctype.h>
+#include <errno.h>
 
 struct input
 {
@@ -16,13 +16,35 @@ struct input
 	char rawInput[512];
 	char* command[1];
 	char* arguments[16];
+	pid_t pid;
+	int done;
 };
 
+/*
+
+pipeline function to set input of one command as output of another
+void pipeline(struct input *in){
+	int fd[2];
+
+	int status = pipe(fd);
+	if(status == 0){
+		if(fork() == 0){
+			close(fd[0]);
+			dup2(fd[1], STDOUT_FILENO);
+			close(fd[1]);
+			execvp(in->command[0], in->arguments);
+		} else {
+			close(fd[1]);
+			dup2(fd[0], STDIN_FILENO);
+			close(fd[0]);
+		}
+	}
+}
+*/
 
 int inRedirect(struct input* in, char* left, char* right)
 {
 	char* tokenize; // used to get the command info from the command line
-
         tokenize = (char*)malloc(512 * sizeof(char)); // allocate the strings
 	strcpy(tokenize, in->rawInput);
 	tokenize = strtok(tokenize, "<"); // tokenize the input to split before and after the <
@@ -30,9 +52,14 @@ int inRedirect(struct input* in, char* left, char* right)
 	while (tokenize != NULL)
 	{
 		if (iterator == 0)
+		{
 			strcpy(left, tokenize);
+		}
 		else if (iterator == 1)
+		{
 			strcpy(right, tokenize);
+			
+		}
 		tokenize = strtok(NULL, "<");
 		tokenize = strtok(tokenize, " ");
 		iterator++;
@@ -58,6 +85,7 @@ int outRedirect(struct input* in, char* left, char* right){
 		else if(iterator == 1)
 			strcpy(right, token);
 		token = strtok(NULL, ">");
+		token = strtok(token, " ");
 		iterator++;
 	}
 
@@ -68,11 +96,27 @@ int outRedirect(struct input* in, char* left, char* right){
 	return 0;
 }
 
+int getCount(char* cmd){
+	int count = 0;
+	char *token = strdup(cmd);
 
-int builtin(struct input* in)
+	token = strtok(cmd, "|");
+	while(token != NULL){
+		count++;
+	}
+	return count;
+}
+
+
+int builtin(struct input* in, int error)
 {
 	if (!strcmp(in->input, "exit"))
 	{
+		if (error == 0)
+		{
+			fprintf(stderr, "Error: active jobs still running\n");
+			return 1;
+		}
 		fprintf(stderr, "Bye...\n");
 		exit(0);
 		return 0;
@@ -96,8 +140,10 @@ int builtin(struct input* in)
 	{
 		int retval = chdir(in->arguments[1]);
 		if (retval == -1)
+		{
 			fprintf(stderr, "Error: no such directory\n");
-
+			return 1;
+		}
 		fprintf(stderr, "+ completed '%s' [%d]\n", in->rawInput, retval);
 		return 1;
 	}
@@ -111,25 +157,48 @@ int main(int argc, char *argv[])
 {
 	int retval;
 	char* cmd[2];
-	size_t size = PATH_MAX * sizeof(char);
+	size_t size = 512 * sizeof(char);
 	pid_t pid;
 	char* left;
 	char* right;
+	//struct input inputs[16]; used to keep the '|' delimited pipe commands
 	
 	cmd[0] = (char*)malloc(size);
 	cmd[1] = NULL;
-	struct input *userIn = malloc(sizeof(struct input));
+	int userInArraycounter = 0;
+	struct input userInArray[50];
 	
 	while (1)
 	{
+		struct input *userIn = malloc(sizeof(struct input));
 		int cont = 0; // USED TO FLAG A CONTINUE IN A NESTED LOOP
+		int background = 0; // USED TO FLAG A BACKGROUND COMMAND
+		int error = 0; // USED TO FLAG AN ERROR
+
+		int test = 1; // = waitpid(-1, &retval, WNOHANG);
+		for (int m = 0; m < userInArraycounter; m++)
+		{
+			if (userInArray[m].done == 1)
+			{
+				continue;
+			}
+			test = waitpid(-1, &retval, WNOHANG);
+			if (test > 0)
+			{
+				userInArray[m].done = 1;
+				fprintf(stderr, "+ completed '%s' [%d]\n", userInArray[m].rawInput, retval);
+			}
+		}
+
 		for (int i = 0; i < 16; i++)
 		{
 			userIn->arguments[i] = NULL;
 		}
+
 		printf("sshell$ ");
-		//printf("arg 1: %s\n", userIn->arguments[1]);
 		int lineSize = getline(&cmd[0], &size - 1, stdin);
+		//int numOfCommands = getCount(cmd[0]);
+
 		if (lineSize < 0)
 		{
 			perror("getline");
@@ -145,39 +214,85 @@ int main(int argc, char *argv[])
 		cmd[0] = strtok(cmd[0], "\n");
 		if (cmd[0] == NULL)
         	{
-			//free(cmd[0]);
-			//free(userIn);
+			free(userIn);
 			continue;
 		}
+		
 		strcpy(userIn->input, cmd[0]);
 		strcpy(userIn->rawInput, userIn->input);
 		userIn->arguments[15] = NULL;
-		int iteratorer = 0;
+		int counter = 0;
 		cmd[0] = strtok(userIn->input, " ");
+
 		while (cmd[0] != NULL)
 		{
-			if (iteratorer == 0)
+			
+			char* temp = (char*)malloc(sizeof(size));
+			temp = cmd[0];
+			temp = strtok_r(temp, "<", &temp);
+			if (temp != NULL && strcmp(temp, cmd[0]))
 			{
-				userIn->command[iteratorer] = cmd[0];
-				userIn->arguments[iteratorer] = cmd[0];
+				left = (char*)malloc(512 * sizeof(char));
+                                right = (char*)malloc(512 * sizeof(char));
+				if (inRedirect(userIn, left, right) != 1)
+				{
+					cont = 2;
+					break;
+				}
+				else
+				{
+					cont = 1;
+				}
 			}
-			else if (iteratorer > 16)
+			char* temp2 = (char*)malloc(sizeof(size));
+			temp2 = cmd[0];
+			if (temp2[strlen(temp2)-1] == *"&" && strcmp(temp2, "&"))
+                        {
+				background = 2;
+				cmd[0][strlen(cmd[0]) -1] = *"";
+				userInArray[userInArraycounter] = *userIn;
+				userInArraycounter++;
+				
+                        }	
+
+			if (counter == 0)
 			{
-				fprintf(stderr, "Error: too many process arguments\n");
-				//free(cmd[0]);
-				//free(userIn);
-				continue;
+				userIn->command[counter] = cmd[0];
+				userIn->arguments[counter] = cmd[0];
+			}
+			else if (counter > 16)
+			{
+				if (!error)
+				{
+					fprintf(stderr, "Error: too many process arguments\n");
+				}
+				error = 1;
 			}
 			else
 			{
-				userIn->arguments[iteratorer] = cmd[0];
-				if (!strcmp(cmd[0], "<")) {
+				if (background == 1)
+				{
+					if (!error)
+					{
+						fprintf(stderr, "Error: mislocated background sign\n");
+					}
+					error = 1;
+				}
+				userIn->arguments[counter] = cmd[0];
+				if (!strcmp(cmd[0], "&"))
+				{
+					userIn->arguments[counter] = NULL;
+					background = 1;
+					userInArray[userInArraycounter] = *userIn;
+					userInArraycounter++;
+				}
+
+				if (!strcmp(cmd[0], "<"))
+				{
 					left = (char*)malloc(512 * sizeof(char));
         				right = (char*)malloc(512 * sizeof(char));
 					if (inRedirect(userIn, left, right) != 1)
 					{
-						//printf("left: %s\n", left);
-						//printf("right: %s\n", right);
 						cont = 2; // NEED TO CHANGE REDIRECTION
 						break;
 					}
@@ -188,30 +303,30 @@ int main(int argc, char *argv[])
 					
 				}
 				else if(!strcmp(cmd[0], ">")) {
-					left = (char*)malloc(512 * sizeof(char));
-        				right = (char*)malloc(512 * sizeof(char));
-					if (outRedirect(userIn, left, right) != 1) {
-						cont = 3;
-						break;
-					}
+                        		left = (char*)malloc(512 * sizeof(char));
+                        		right = (char*)malloc(512 * sizeof(char));
+                        		if (outRedirect(userIn, left, right) != 1) {
+                               			cont = 3;
+                                		break;
+                        		}
 					else {
 						cont = 1;
 					}
-				}
+                		}
 			}
 			cmd[0] = strtok(NULL, " ");
-			iteratorer++;
+			counter++;
 		}
 
-		if (builtin(userIn) == 1 || cont == 1)
+		if (builtin(userIn, test) == 1 || cont == 1 || error == 1 || userIn->command[0] == NULL)
 		{
+			free(userIn);
 			continue;
 		}		
 
 		pid = fork();
 		if(pid == 0)
 		{
-			//printf("cont: %d\n", cont);
 			if (cont == 2)
 			{
 				int inFile = open(right, O_RDONLY);      
@@ -230,14 +345,13 @@ int main(int argc, char *argv[])
 					{
 						userIn->arguments[j] = NULL;
 					}
-					//printf("%d: %s\n", j, userIn->arguments[j]);
 				}
-
 				execvp(userIn->command[0], userIn->arguments);
 				exit(1);
 
 			}
-			else if(cont == 3) {
+
+                        else if(cont == 3) {
 				int outFile = open(right, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 				if(outFile == -1)
 				{
@@ -266,9 +380,47 @@ int main(int argc, char *argv[])
 		}
 		else if(pid != 0)
 		{
-			waitpid(-1, &retval, 0);
-			fprintf(stderr, "+ completed '%s' [%d]\n", userIn->rawInput, retval);
+			userIn->pid = pid;
+			int status;
+			if (background == 0)
+			{
+				status = waitpid(pid, &retval, 0);
+				for (int m = 0; m < userInArraycounter; m++)
+                                {
+                                        if (userInArray[m].done == 1)
+                                        {
+                                                continue;
+                                        }
+                                        test = waitpid(-1, NULL, WNOHANG);
+                                        if (test > 0)
+                                        {
+                                                userInArray[m].done = 1;
+                                                fprintf(stderr, "+ completed '%s' [%d]\n", userInArray[m].rawInput, retval);
+                                        }
+                                }
+				if  (retval == 256)
+				{
+					fprintf(stderr, "Error: command not found\n");
+				}
+				else if (status == pid)
+				{
+					fprintf(stderr, "+ completed '%s' [%d]\n", userIn->rawInput, retval);
+				}
+			}
+			else if (background >= 1)
+                        {
+                                status = waitpid(-1, &retval, WNOHANG);
+                                if  (retval == 256)
+                                {
+                                        fprintf(stderr, "Error: command not found\n");
+                                }
+                                else if (status > 0)
+                                {
+                                        fprintf(stderr, "+ completed '%s' [%d]\n", userIn->rawInput, retval);
+                                }
+                        }
 		}
+		free(userIn);
 	}
 	return EXIT_SUCCESS;
 }
